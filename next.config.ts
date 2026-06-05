7e46f358dd09c23079b725e4054e1133f124f95f
UrveshVernekar <urveshvernekar8@gmail.com>
2026-06-05 17:25:03 +0530

    Add origins to config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['10.0.11.250', '10.0.11.150', '127.0.0.1', 'localhost'],
 };
 
 export default nextConfig;
